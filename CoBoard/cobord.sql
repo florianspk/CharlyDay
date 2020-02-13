6a263762dee056a99734cff73f339a24efd375de
Nathan Maurice <Nathan.maurice8@etu.univ-lorraine.fr>
2020-02-13 12:24:57 +0100

    bdd

---
+++ CoBoard/cobord.sql
@@ -0,0 +1,141 @@
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le :  jeu. 13 fév. 2020 à 11:23
+-- Version du serveur :  5.7.26
+-- Version de PHP :  7.2.18
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `cobord`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `alerte`
+--
+
+DROP TABLE IF EXISTS `alerte`;
+CREATE TABLE IF NOT EXISTS `alerte` (
+  `id_alerte` int(11) NOT NULL,
+  `id_besoin` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `besoin`
+--
+
+DROP TABLE IF EXISTS `besoin`;
+CREATE TABLE IF NOT EXISTS `besoin` (
+  `role_id` int(11) NOT NULL,
+  `creneau_id` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `creneau`
+--
+
+DROP TABLE IF EXISTS `creneau`;
+CREATE TABLE IF NOT EXISTS `creneau` (
+  `heure` date NOT NULL,
+  `jour` date NOT NULL,
+  `semaine` varchar(3) COLLATE utf8_general_mysql500_ci NOT NULL,
+  `id_cycle` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `cycle`
+--
+
+DROP TABLE IF EXISTS `cycle`;
+CREATE TABLE IF NOT EXISTS `cycle` (
+  `id_cycle` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_general_mysql500_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `role`
+--
+
+DROP TABLE IF EXISTS `role`;
+CREATE TABLE IF NOT EXISTS `role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `label` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `role`
+--
+
+INSERT INTO `role` (`id`, `label`) VALUES
+(1, 'Caissier titulaire'),
+(2, 'Caissier assistant'),
+(3, 'Gestionnaire de vrac titulaire'),
+(4, 'Gestionnaire de vrac assistant'),
+(5, 'Chargé d\'accueil titulaire'),
+(6, 'Chargé d\'accueil assistant');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nom` varchar(30) NOT NULL,
+  `prenom` varchar(30) NOT NULL,
+  `login` varchar(30) NOT NULL,
+  `mdp` varchar(30) NOT NULL,
+  `mail` varchar(50) NOT NULL,
+  `tel` varchar(15) NOT NULL,
+  `permanance` int(11) NOT NULL,
+  `absences` int(11) NOT NULL,
+  `photo` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `nom`, `prenom`, `login`, `mdp`, `mail`, `tel`, `permanance`, `absences`, `photo`) VALUES
+(1, 'Cassandre', '', '', '', '', '', 0, 0, ''),
+(2, 'Achille', '', '', '', '', '', 0, 0, ''),
+(3, 'Calypso', '', '', '', '', '', 0, 0, ''),
+(4, 'Bacchus', '', '', '', '', '', 0, 0, ''),
+(5, 'Diane', '', '', '', '', '', 0, 0, ''),
+(6, 'Clark', '', '', '', '', '', 0, 0, ''),
+(7, 'Helene', '', '', '', '', '', 0, 0, ''),
+(8, 'Jason', '', '', '', '', '', 0, 0, ''),
+(9, 'Bruce', '', '', '', '', '', 0, 0, ''),
+(10, 'Pénélope', '', '', '', '', '', 0, 0, ''),
+(11, 'Ariane', '', '', '', '', '', 0, 0, ''),
+(12, 'Lois', '', '', '', '', '', 0, 0, '');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
