113d4eca5de038912ebd986904ee6b9520b0c0f1
Krell Lucas <lucaskrell@hotmail.fr>
2020-02-13 19:58:55 +0100

    fix

--- CoBoard/src/vues/VueClient.php
+++ CoBoard/src/vues/VueClient.php
@@ -48,7 +48,7 @@ class VueClient
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="Front/css/sb-admin-2.css" rel="stylesheet">
+  <link href="Front/css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
 <body class="bg-gradient-primary">
