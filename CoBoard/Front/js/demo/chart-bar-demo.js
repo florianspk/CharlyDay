20a8bacaf8d46b43754c13ac3fc00b69eb27e4e5
Zappone <arthur.zappone@etu.univ-lorraine.fr>
2020-02-13 19:38:48 +0100

    ajout ^photot

--- CoBoard/Front/js/demo/chart-bar-demo.js
+++ CoBoard/Front/js/demo/chart-bar-demo.js
@@ -32,7 +32,7 @@ var ctx = document.getElementById("myBarChart");
 var myBarChart = new Chart(ctx, {
   type: 'bar',
   data: {
-    labels: ["January", "February", "March", "April", "May", "June"],
+    labels: ["Janvier", "Fevrier", "Mars", "Avril", "Mai", "Juin"],
     datasets: [{
       label: "Revenue",
       backgroundColor: "#4e73df",
@@ -54,7 +54,7 @@ var myBarChart = new Chart(ctx, {
     scales: {
       xAxes: [{
         time: {
-          unit: 'month'
+          unit: 'Mois'
         },
         gridLines: {
           display: false,
