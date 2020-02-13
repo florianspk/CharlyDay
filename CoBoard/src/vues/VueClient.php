74c03fcbb1f3a47e6a351ac9923ee8585a8da43c
Krell Lucas <lucaskrell@hotmail.fr>
2020-02-13 19:24:07 +0100

    permnanence fonctionnelle

--- CoBoard/src/vues/VueClient.php
+++ CoBoard/src/vues/VueClient.php
@@ -44,11 +44,11 @@ class VueClient
   <title>Mon compte</title>
 
   <!-- Custom fonts for this template-->
-  <link href="../Front/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="Front/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="../Front/css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="Front/css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
 
@@ -57,21 +57,21 @@ class VueClient
   $content
   
   <!-- Bootstrap core JavaScript-->
-  <script src="../Front/vendor/jquery/jquery.min.js"></script>
-  <script src="../Front/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="vendor/jquery/jquery.min.js"></script>
+  <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
   <!-- Core plugin JavaScript-->
-  <script src="../Front/vendor/jquery-easing/jquery.easing.min.js"></script>
+  <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
 
   <!-- Custom scripts for all pages-->
-  <script src="../Front/js/sb-admin-2.min.js"></script>
+  <script src="Front/js/sb-admin-2.min.js"></script>
 
   <!-- Page level plugins -->
-  <script src="../Front/vendor/chart.js/Chart.min.js"></script>
+  <script src="vendor/chart.js/Chart.min.js"></script>
 
   <!-- Page level custom scripts -->
-  <script src="../Front/js/demo/chart-area-demo.js"></script>
-  <script src="../Front/js/demo/chart-pie-demo.js"></script>
+  <script src="Front/js/demo/chart-area-demo.js"></script>
+  <script src="Front/js/demo/chart-pie-demo.js"></script>
 </body>
 </html>
 END;
